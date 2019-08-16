d7de8c07aea503d38f58d6f5ecf125a7b110e302
Matthew Nelson <05nelsonm@protonmail.com>
2019-08-16 12:09:18 -0400

    added recyclerview layout

--- app/src/main/java/com/lambdaschool/sprint2challenge/activity/ShoppingListActivity.kt
+++ app/src/main/java/com/lambdaschool/sprint2challenge/activity/ShoppingListActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lambdaschool.sprint2challenge.R
 
-class MainActivity : AppCompatActivity() {
+class ShoppingListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping_list)
     }
 }
